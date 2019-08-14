e0693b530d89d7798c8e7262a763f471a6dda0df
Ekzaton <ekzaton@gmail.com>
2019-08-14 12:27:32 +0500

    Дополнил оптимизацию

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,26 @@ var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
 var del = require("del");
 var server = require("browser-sync").create();
 
+//Оптимизация CSS
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(csso())
-    .pipe(rename("style.min.css"))
+    .pipe(rename({suffix: '.min'}))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
+//Оптимизация изображений
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
@@ -40,41 +41,53 @@ gulp.task("images", function () {
     .pipe(gulp.dest("source/img"));
 });
 
+//Создание .webp из .png и .jpg
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
 
+//Создание .svg-спрайта из отдельных .svg
 gulp.task("sprite", function () {
   return gulp.src("source/img/{htmlacademy.svg,icon-*.svg}")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
+    .pipe(svgstore())
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 });
 
+//Оптимизация HTML
 gulp.task("html", function () {
   return gulp.src("source/*.html")
+    .pipe(htmlmin())
     .pipe(gulp.dest("build"));
 });
 
+//Оптимизация JS
+gulp.task("js", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest("build/js"));
+});
+
+//Копирование шрифтов и изображений в папку сборки проекта
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**"
   ], {
     base: "source"
   })
   .pipe(gulp.dest("build"));
 });
 
+//Удаление всех файлов из папки сборки проекта
 gulp.task("clean", function () {
   return del("build");
 });
 
+//Запуск сервера
 gulp.task("server", function () {
   server.init({
     server: "build/",
@@ -89,10 +102,14 @@ gulp.task("server", function () {
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
+//Перезапуск сервера
 gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+//Сборка проекта
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "js"));
+
+//Сборка проекта и запуск сервера
 gulp.task("start", gulp.series("build", "server"));
